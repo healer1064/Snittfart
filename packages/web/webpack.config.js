f2c90e3f2048601c32f36b7fafd9696130fc0cb3
healer1064 <healer1064729@gmail.com>
2018-12-23 23:12:28 +0100

    Use webpack
    cra did not work to well with monorepo yarn workspaces
    jest has also been painful

---
+++ packages/web/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  entry: {
+    app: path.resolve(__dirname, 'src/index.js'),
+    vendor: ['react', 'react-dom']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: `bundle-[chunkHash].js`,
+    chunkFilename: `[name]-[chunkHash].js`,
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(
+        process.env.NODE_ENV || 'development'
+      )
+    }),
+    new HtmlWebpackPlugin({ template: 'index.html.ejs' }),
+    new CopyWebpackPlugin([{ from: path.resolve(__dirname, 'public') }])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: [
+          path.join(__dirname, 'src'),
+          path.join(__dirname, '..', 'shared', 'src')
+        ],
+        use: [{ loader: 'babel-loader' }]
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    quiet: false,
+    noInfo: false,
+    stats: {
+      assets: true,
+      version: false,
+      hash: false,
+      timings: false,
+      chunks: false,
+      chunkModules: true
+    }
+  }
+};
