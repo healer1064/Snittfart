e97a0b1c1beaf1edd4da2cb44ffdad480748aa46
healer1064 <healer1064729@gmail.com>
2020-02-12 21:56:02 +0100

    Upgrade web deps

--- packages/web/babel.config.js
+++ packages/web/babel.config.js
@@ -1,4 +1,17 @@
 module.exports = {
-  presets: ['react-app'],
-  plugins: ['babel-plugin-react-native-web']
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        useBuiltIns: false,
+        modules: false
+      }
+    ],
+    '@babel/preset-react',
+    '@babel/preset-flow'
+  ],
+  plugins: [
+    'babel-plugin-react-native-web',
+    '@babel/plugin-proposal-class-properties'
+  ]
 };
