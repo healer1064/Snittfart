baf7e8251ee9e959896697cc24abe6a7a3beb871
healer1064 <healer1064729@gmail.com>
2021-05-03 21:01:13 +0200

    Add more wrappers

--- src/index.tsx
+++ src/index.tsx
@@ -3,26 +3,32 @@ import '@devmoods/ui/dist/styles.css';
 
 import './index.css';
 
-import { ThemeProvider, createTheme } from '@devmoods/ui';
+import { ErrorBoundary, ThemeProvider, createTheme } from '@devmoods/ui';
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 
 import App from './App';
 
 const theme = createTheme({
   colors: {
     primary: '#d75648',
-    focusRing: '#7EB1F6',
     formsBorderRadius: '6px',
     formsBorderWidth: '1px',
     formsBorder: '#e4e4e4',
     formsBorderBackground: '#f1f2f3',
   },
+  focusRing: {
+    color: '#e9958c',
+  },
 });
 
-ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>,
+render(
+  <React.StrictMode>
+    <ErrorBoundary>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ErrorBoundary>
+  </React.StrictMode>,
   document.getElementById('root')
 );
